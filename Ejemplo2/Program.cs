d808de67f0dae8bc560fbc60d6718b1dd2820107
Claudio Lages <claudio.lages@globalprocessing.com.ar>
2024-05-10 10:03:53 -0300

    Fix ejemplo2

--- Ejemplo2/Program.cs
+++ Ejemplo2/Program.cs
@@ -21,58 +21,47 @@ namespace Ejemplo2
             //codMarca 1 a 10
             //precio
 
-            
-            string sexo = "";
 
-            Console.WriteLine("Ingresa tu sexo M o F");
-            sexo = Console.ReadLine();
-            Console.WriteLine("\n\n\nSEXO: " + sexo);
+            Articulo[] articulos = new Articulo[10];
 
-
-            /*
-           Articulo[] articulos = new Articulo[10];
-
-           for (int i = 0; i<1; i++)
-           {
-               articulos[i] = new Articulo();
-               Console.WriteLine("Ingrese Producto " + (i+1));
+            for (int i = 0; i < 2; i++)
+            {
+                articulos[i] = new Articulo();
+                Console.WriteLine("Ingrese Producto " + (i + 1));
                 Console.WriteLine("Codigo: ");
                 articulos[i].CodigoArticulo = int.Parse(Console.ReadLine());
                 Console.WriteLine("Marca: ");
                 articulos[i].CodigoMarca = int.Parse(Console.ReadLine());
                 Console.WriteLine("Precio: ");
-                */
+                articulos[i].Precio = float.Parse(Console.ReadLine());
+            }
+
+            Console.WriteLine("Datos ingresados:");
+            for (int i = 0; i < 2; i++)
+            {
+                Console.WriteLine($"Producto {i + 1}:");
+                Console.WriteLine($"Codigo: {articulos[i].CodigoArticulo}");
+                Console.WriteLine($"Marca: {articulos[i].CodigoMarca}");
+                Console.WriteLine($"Precio: {articulos[i].Precio}");
+                Console.WriteLine();
+            }
+
+            Venta venta = new Venta();
+            Console.WriteLine("Ingrese la venta:");
+            Console.WriteLine("Codigo de Cliente:");
+            venta.CodigoCliente = int.Parse(Console.ReadLine());
 
 
-
-            // articulos[i].Precio = float.Parse(Console.ReadLine());
+            while (venta.CodigoCliente != 0)
+            {
+                Console.WriteLine("Codigo de Articulo:");
+                venta.CodigoArticulo = int.Parse(Console.ReadLine());
+                Console.WriteLine("Cantidad");
+                venta.Cantidad = int.Parse(Console.ReadLine());
+                Console.WriteLine("Ingrese la venta:");
+                Console.WriteLine("Codigo de Cliente:");
+                venta.CodigoCliente = int.Parse(Console.ReadLine());
+            }
         }
-
-        /*  Console.WriteLine("Datos ingresados:");
-          for (int i = 0; i < 2; i++)
-          {
-              Console.WriteLine($"Producto {i + 1}:");
-              Console.WriteLine($"Codigo: {articulos[i].CodigoArticulo}");
-              Console.WriteLine($"Marca: {articulos[i].CodigoMarca}");
-              Console.WriteLine($"Precio: {articulos[i].Precio}");
-              Console.WriteLine();
-          }
-
-          Venta venta = new Venta();
-          Console.WriteLine("Ingrese la venta:");
-          Console.WriteLine("Codigo de Cliente:");
-          venta.CodigoCliente = int.Parse(Console.ReadLine());
-
-
-          while(venta.CodigoCliente != 0)
-          {
-              Console.WriteLine("Codigo de Articulo:");
-              venta.CodigoArticulo = int.Parse(Console.ReadLine());
-              Console.WriteLine("Cantidad");
-              venta.Cantidad = int.Parse(Console.ReadLine());
-              Console.WriteLine("Ingrese la venta:");
-              Console.WriteLine("Codigo de Cliente:");
-              venta.CodigoCliente = int.Parse(Console.ReadLine());
-          }*/
     }
 }
